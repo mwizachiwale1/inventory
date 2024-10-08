a8ee4c2aec6863f549f31d76686b543e98540a62
mwizachiwale1 <47919799+mwizachiwale1@users.noreply.github.com>
2024-10-08 13:53:02 +0200

    Update wsgi.py
    Changes

--- inventory_project/wsgi.py
+++ inventory_project/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
 
+# Add your project directory to the sys.path
+project_home = '/home/mwizachiwale/inventory'
+if project_home not in sys.path:
+    sys.path = [inventory_project] + sys.path
+
+# Set the settings module for your Django project
+os.environ['DJANGO_SETTINGS_MODULE'] = 'inventory_project.settings'
+
+# Activate the virtual environment
+activate_env = '/home/mwizachiwale/venv/bin/activate_this.py'
+with open(activate_env) as f:
+    exec(f.read(), dict(__file__=activate_env))
+
+# Get the WSGI application for Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings')
-
 application = get_wsgi_application()
